c4c8bf703eaf996a8a1ef3455fcc613db70486ae
unknown <wangyudanberry@gmail.com>
2017-09-12 14:44:06 +1000

    finish feature scaling

--- src/feature_scaling.clj
+++ src/feature_scaling.clj
@@ -3,13 +3,17 @@
 
 (defn- scale-column [matrix column]
   (let [mean (/ (matrix/esum column) (matrix/ecount column))
-        max (matrix/emax column)
-        scaled-column (matrix/emap #(/ (- % mean) max) column)]
+        range (- (matrix/emax column) (matrix/emin column))
+        scaled-column (matrix/emap #(/ (- % mean) range) column)]
     (matrix/join-along 1 matrix scaled-column)))
 
-;create an empty matrix (nil) to combine scaled columns
-(defn scale [X]
+(defn- scale-matrix [X]
   (reduce
     scale-column
     (matrix/broadcast [nil] [(matrix/row-count X) 1])
     (matrix/columns X)))
+
+(defn scale [X]
+  (let [scaled-matrix (scale-matrix X)
+        last-column-index (dec (matrix/column-count scaled-matrix))]
+    (matrix/matrix (matrix/submatrix scaled-matrix 1 [1 last-column-index]))))
