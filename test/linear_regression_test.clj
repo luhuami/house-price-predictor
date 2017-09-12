2e20a8224f37d98f2686fbace5909914c0af8c14
unknown <wangyudanberry@gmail.com>
2017-09-12 23:13:39 +1000

    add code to read data with only one feature

--- test/linear_regression_test.clj
+++ test/linear_regression_test.clj
@@ -3,10 +3,10 @@
               [clojure.string :refer :all]
               [clojure.core.matrix :as matrix]
               [linear-regression :as lr]
-              [utils.feature-scaling :as fs]))
+              [utils.feature-normalize :as fs]))
 
 
-(def row-data (slurp "test/resource/data.txt"))
+(def row-data (slurp "test/resource/linear/regression/multi-feature.txt"))
 
 (def row-arr (split-lines row-data))
 
@@ -15,18 +15,21 @@
 (def arr1 (flatten arr))
 
 (def arr2 (map #(Integer/parseInt %) arr1))
+;(def arr2 (map #(Double/parseDouble %) arr1))
 
 (def x-list (partition 2 3 arr2))
+;(def x-list (partition 1 2 arr2))
 
 (def X (matrix/matrix x-list))
 
 (def y-list (map last (partition 3 arr2)))
+;(def y-list (map last (partition 2 arr2)))
 
 (def y (matrix/matrix (map vector y-list)))
 
 ;(lr/perform-batch-gradient-decent (fs/scale X) y 0.01 50)
 
-(lr/perform-batch-gradient-decent (fs/scale X) (fs/scale y) 0.01 50)
+(lr/perform-batch-gradient-decent (fs/normalize X) y 0.01 50)
 
 
 
