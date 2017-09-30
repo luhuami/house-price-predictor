08d4c6feaacc8b13b32a6a48922a600e60deccdd
unknown <wangyudanberry@gmail.com>
2017-09-30 23:23:54 +1000

    add functions used for forward-propagation

--- project.clj
+++ project.clj
@@ -5,7 +5,8 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [dk.ative/docjure "1.11.0"]
-                 [net.mikera/vectorz-clj "0.47.0"]]
+                 [net.mikera/vectorz-clj "0.47.0"]
+                 [org.clojure/math.numeric-tower "0.0.4"]]
   :main ^:skip-aot core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/neural_networks.clj
+++ src/neural_networks.clj
@@ -1,5 +1,6 @@
 (ns neural-networks
-  (:require [clojure.core.matrix :as matrix]))
+  (:require [clojure.core.matrix :as matrix]
+            [clojure.math.numeric-tower :as math]))
 
 ;number of neural in each layer excluding bias node. layer num starts from 1.
 ;[400 25 10] represents a 3 layers neural networks.
@@ -20,11 +21,31 @@
 (defn- init-theta [structure]
   (vec (repeat (calc-total-theta-num structure) 0)))
 
-(defn- get-theta-matrix [layer-num theta structure]
+(defn- get-theta-matrix [layer-num theta-vec structure]
   (if (and (< layer-num (count structure)) (< 0 layer-num))
     (let [previous-layers-theta-num (calc-total-theta-num (subvec structure 0 layer-num))
           theta-num-this-layer (get structure (dec layer-num))
           theta-num-next-layer (get structure layer-num)
           theta-num (calc-theta-num-for-layer theta-num-this-layer theta-num-next-layer)
-          theta-vec (subvec theta previous-layers-theta-num (+ theta-num previous-layers-theta-num))]
+          theta-vec (subvec theta-vec previous-layers-theta-num (+ theta-num previous-layers-theta-num))]
       (matrix/reshape theta-vec [theta-num-next-layer (inc theta-num-this-layer)]))))
+
+(defn- sigmoid [z]
+  (->> z
+       (math/expt Math/E)
+       (/ 1)
+       (+ 1)
+       (/ 1)))
+
+;calculate next layer's activation with bias unit.
+; Given activation should include bias unit so it should be (n+1)*1.
+(defn- calc-next-activation [activation theta]
+  (let [z (matrix/mmul theta activation)
+        a (matrix/emap sigmoid z)]
+    (matrix/join-along 1 [1] a)))
+
+(defn- convert-x [x-vec]
+  (matrix/transpose [x-vec]))
+
+(defn- forward-propagation [x theta]
+  )
