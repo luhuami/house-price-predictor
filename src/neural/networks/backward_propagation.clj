24ba1aabd08325de0d1f0311ac107c42e3c11f77
unknown <wangyudanberry@gmail.com>
2017-10-03 23:38:11 +1100

    add code to calculate delta for all layers

--- src/neural/networks/backward_propagation.clj
+++ src/neural/networks/backward_propagation.clj
@@ -6,19 +6,39 @@
   (let [m (matrix/new-matrix (first dimension-vec) (last dimension-vec))]
     (matrix/fill m 1)))
 
-(defn- calc-delta1 [next-layer-delta theta activation]
-  (let [temp (matrix/mmul (matrix/transpose theta) next-layer-delta)
+(defn- calc-layer-delta [delta-list theta-activation-pair]
+  (let [next-layer-delta (first delta-list)
+        theta (first theta-activation-pair)
+        activation (second theta-activation-pair)
+        temp (matrix/mmul (matrix/transpose theta) next-layer-delta)
         ones (create-ones (matrix/shape activation))]
-    (matrix/emul temp activation (matrix/sub ones activation))))
+    (conj delta-list (matrix/emul temp activation (matrix/sub ones activation)))))
 
-;TODO:merge with above function?
-(defn- calc-delta [delta-vec theta-activation-pair]
-  (let [next-layer-delta (last delta-vec)
-        this-layer-delta (calc-delta1 next-layer-delta (first theta-activation-pair) (second theta-activation-pair))]
-    (conj delta-vec this-layer-delta)))
+(defn- generate-theta-activation-pair [theta-vec activation-vec]
+  (let [t-a-pair (partition 2 (interleave theta-vec (drop-last activation-vec)))]
+    (drop-last (reverse t-a-pair))))
 
-(defn backward-propagation [activation-vec theta-vec, y]
-  (reverse (reduce
-             calc-delta
-             [(matrix/sub (last activation-vec) y)]
-             (partition 2 (interleave theta-vec (drop-last activation-vec))))))
+;Length of theta-vec should be l-1.Length of activation-vec should be l,
+(defn- calc-delta-list [theta-vec activation-vec y]
+  (reduce
+    calc-layer-delta
+    (list (matrix/sub (last activation-vec) y))
+    (generate-theta-activation-pair theta-vec activation-vec)))
+
+;TODO: delta-matrix a matrix or vector?
+(defn- remove-bias-delta [delta-matrix]
+  (matrix/submatrix delta-matrix 0 [1 (dec (matrix/row-count delta-matrix))]))
+
+(defn- remove-bias-unit-delta [delta-list]
+  (map-indexed
+    #((if (< %1 (dec (count delta-list))) (remove-bias-delta %2)))
+    delta-list))
+
+(defn- calc-one-step-gradient-decent [next-layer-delta activation]
+  (matrix/mmul next-layer-delta (matrix/transpose activation)))
+
+(defn calc-one-step-gradient-decent-for-all-layers [theta-vec activation-vec y]
+  (let [raw-delta-list (calc-delta-list theta-vec activation-vec y)
+        delta-list (remove-bias-unit-delta raw-delta-list)
+        a (drop-last activation-vec)]
+    (map calc-one-step-gradient-decent delta-list a)))
