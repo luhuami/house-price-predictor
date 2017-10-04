bd14ac3938973a9ff702f835f3264d506603efee
unknown <wangyudanberry@gmail.com>
2017-10-04 23:53:08 +1100

    sum up deltas

--- src/neural/networks/train_neural_networks.clj
+++ src/neural/networks/train_neural_networks.clj
@@ -1,6 +1,7 @@
 (ns neural.networks.train-neural-networks
   (:require [clojure.core.matrix :as matrix]
-            [neural.networks.forward-propagation :as fp]))
+            [neural.networks.forward-propagation :as fp]
+            [neural.networks.backward-propagation :as bp]))
 
 ;number of neural in each layer excluding bias node. layer num starts from 1.
 ;[400 25 10] represents a 3 layers neural networks.
@@ -8,15 +9,28 @@
 ;output layer has 10 nodes (output).
 (def neural-networks-structure [400 25 10])
 
-(def theta-vec [])
-
 (defn- calc-theta-seq [structure]
   (reverse (reduce
              #(conj %1 (matrix/new-matrix (second %2) (inc (first %2))))
              (list)
              (partition 2 1 structure))))
 
-(defn calc [x structure]
-  (let [theta-seq (calc-theta-seq structure)
-        activation-seq (fp/calc-activation-vec x theta-seq)]
-    ))
+(defn- calc-delta [theta-seq]
+  (fn [delta-seq train-pair]
+    (let [activation-seq (fp/calc-activation-seq (first train-pair) theta-seq)
+          new-delta-seq (bp/calc-one-delta-for-all-layers theta-seq activation-seq (last train-pair))]
+      (map (matrix/add %1 %2) delta-seq new-delta-seq))))
+
+;X matrix of training set
+;Y matrix of result set
+;theta-seq can be initial theta sequence
+;TODO: X Y can be vector of vecotrs no need to be a matrix?
+(defn- calc-deltas [structure X Y theta-seq]
+  (let [initial-delta-seq (calc-theta-seq structure)
+        train-pairs (partition 2 (interleave (matrix/rows X) (matrix/rows Y)))]
+    (reduce
+      (calc-delta theta-seq)
+      initial-delta-seq
+      train-pairs)))
+
+;TODO: divide by m and add lambda theta
