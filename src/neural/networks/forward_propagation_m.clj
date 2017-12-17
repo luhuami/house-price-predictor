1cce87db5166db0fe638ba72df442782dbdf5c6c
unknown <wangyudanberry@gmail.com>
2017-12-17 15:26:39 +1100

    refactor for easier unit test

--- src/neural/networks/forward_propagation_m.clj
+++ src/neural/networks/forward_propagation_m.clj
@@ -6,18 +6,22 @@
 (defn- add-bias-to-activation [a]
   (matrix/join-along 1 (utils/create-matrix-with-value [(matrix/row-count a) 1] 1) a))
 
-;Calculate next layer's activation, attach bias unit and save it to a vector.
-;Given activations must include the bias unit so is a m*(n+1) matrix.
-(defn- calc-next-activation [activation-vec theta]
-  (let [z (matrix/mmul (last activation-vec) (matrix/transpose theta))
+;Calculate next layer's activation and attach bias unit.
+;The given activation must include the bias unit so it is a m*(n+1) matrix.
+(defn- calc-next-activation [current-activation theta]
+  (let [z (matrix/mmul current-activation (matrix/transpose theta))
         a (matrix/emap sigmoid z)]
-    (conj activation-vec (add-bias-to-activation a))))
+    (add-bias-to-activation a)))
 
-;X is training data, m*n matrix
+(defn- gen-activations [activation-vec theta]
+  (let [current-activation (last activation-vec)]
+    (conj activation-vec (calc-next-activation current-activation theta))))
+
+;X is training data, a m*n matrix
 ;Return [a1 a2 a3 ... aL]
 (defn calc-activation-seq [X theta-seq]
   (reduce
-    calc-next-activation
+    gen-activations
     [(add-bias-to-activation X)]
     theta-seq))
 
