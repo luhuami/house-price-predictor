d627c59d80699a441b9c4aaf469329741b73e218
unknown <wangyudanberry@gmail.com>
2017-12-22 16:19:28 +1100

    add test for gen-activations

--- test/neural/networks/forward_propagation_m_test.clj
+++ test/neural/networks/forward_propagation_m_test.clj
@@ -1,11 +1,12 @@
 (ns neural.networks.forward-propagation-m-test
   (:require [clojure.test :refer :all]
-            [neural.networks.forward-propagation-m :refer :all]
-            [logistic-regression :as lr]))
+            [neural.networks.forward-propagation-m :as fp]
+            [logistic-regression :as lr]
+            [clojure.core.matrix :as matrix]))
 
 (deftest test-add-bias-to-activation
   (testing "add-bias-to-activation should add a new column of 1 to a given matrix"
-    (let [add-bias #'neural.networks.forward-propagation-m/add-bias-to-activation]
+    (let [add-bias #'fp/add-bias-to-activation]
       (is (= [[1 2 5] [1 3 6] [1 4 7]] (add-bias [[2 5] [3 6] [4 7]])))
       (is (= [[1 2 5 0] [1 3 6 4] [1 4 7 1]] (add-bias [[2 5 0] [3 6 4] [4 7 1]])))
       (is (= [[1 1] [1 2] [1 3]] (add-bias [[1] [2] [3]])))
@@ -16,9 +17,10 @@
 (defn- sigmoid-mock [m]
   m)
 
+;TODO
 (deftest test-calc-next-activation
   (testing ""
-    (let [calc-next-activation #'neural.networks.forward-propagation-m/calc-next-activation]
+    (let [calc-next-activation #'fp/calc-next-activation]
       (is (= [[1 38.0 59.0 26.0 37.0] [1 47.0 78.0 41.0 42.0] [1 54.0 91.0 49.0 47.0]]
              (with-redefs-fn
                {#'lr/sigmoid sigmoid-mock}
@@ -27,3 +29,13 @@
              (with-redefs-fn
                {#'lr/sigmoid sigmoid-mock}
                #(calc-next-activation [[1 2 4] [1 1 3]] [[1 2 3] [3 2 1] [1 1 4] [2 1 2]])))))))
+
+(defn- calc-next-activation-mock[a theta]
+  (matrix/mmul a theta))
+
+(deftest test-gen-activations
+  (testing ""
+    (let [gen-activations #'fp/gen-activations]
+      (with-redefs-fn
+        {#'fp/calc-next-activation calc-next-activation-mock}
+        #(is (= (gen-activations [[[1 3] [1 2]]] [[3 1] [4 1]]) [[[1 3] [1 2]] [[15.0 4.0] [11.0 3.0]]]))))))
