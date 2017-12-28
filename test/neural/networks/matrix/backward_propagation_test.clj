3e289284fe95d9c8f5e6ea0a2da9a09a04e942ee
luhuami <freedolphin212@gmail.com>
2017-12-28 17:31:19 +1100

    change data and add test for calc-theta-directive

--- test/neural/networks/matrix/backward_propagation_test.clj
+++ test/neural/networks/matrix/backward_propagation_test.clj
@@ -41,12 +41,17 @@
 
 (def t1 [[1 1 0 2] [2 0 1 1]])
 (def t2 [[3 2 1]])
-(def ac1 [[1 2 1 0] [2 1 1 0] [1 3 0 1]])
-(def ac2 [[1 1 2] [2 2 1] [1 1 3]])
+(def ac1 [[1.5 1.5 3 0] [2 1 1 0] [1 3 0 1]])
+(def ac2 [[1 1 2] [2 3 1] [1 1 3]])
 (def ac3 [[3] [1] [4]])
 (def Y [[2] [3] [1]])
 
 (deftest test-calc-big-deltas
   (testing ""
     (is (= (#'bp/calc-big-deltas (list t1 t2) (list ac1 ac2 ac3) Y)
-           (list [[16.0 8.0 8.0 0.0] [-20.0 -58.0 -2.0 -18.0]] [[0.0 0.0 9.0]])))))
+           (list [[48.0 24.0 24.0 0.0] [-21.0 -57.0 -6.0 -18.0]] [[0.0 -2.0 9.0]])))))
+
+(deftest test-calc-theta-directives
+  (testing ""
+    (is (= (bp/calc-theta-directives (list t1 t2) (list ac1 ac2 ac3) Y 1.5))
+        (list [[16.0 8.5 8.0 1.0] [-7.0 -19.0 -1.5 -5.5]] [[1.5 1.0 3.5]]))))
